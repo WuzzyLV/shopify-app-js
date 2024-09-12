229b82d368dc305faad56b4f7b0ce5a84181c9e1
Ryan DJ Lee <ryandjlee@Ryans-MacBook-Pro.local>
2024-09-12 15:27:54 -0400

    Add documentation for Scopes API
    Co-authored-by: Jonathan J Chang <jonathan.j.chang@shopify.com>

--- packages/apps/shopify-app-remix/src/server/authenticate/admin/scope/types.ts
+++ packages/apps/shopify-app-remix/src/server/authenticate/admin/scope/types.ts
@@ -1,14 +1,226 @@
+/**
+ * The Scopes API enables embedded apps and extensions to request merchant consent for access scopes.
+ */
 export interface ScopesApiContext {
+  /**
+   * Queries Shopify for the scopes for this app on this shop
+   *
+   * @returns The scope details.
+   *
+   * @example
+   * <caption>Query for scopes details.</caption>
+   * <description>Call `scopes.query` to get scope details.</description>
+   * ```ts
+   * // /app/routes/app.query.tsx
+   *import type { ActionFunctionArgs } from "@remix-run/node";
+   *import { json } from "@remix-run/node";
+   *
+   *import { authenticate } from "../shopify.server";
+   *
+   *export async function action({
+   *  request,
+   *}: ActionFunctionArgs) {
+   *  const { scopes } = await authenticate.admin(request);
+   *
+   *  const response = await scopes.query();
+   *  return json({scopesDetail : response});
+   *}
+   * ```
+   * ```ts
+   * // /app/routes/**\/*.tsx
+   *const handleQueryClick = () => {
+   *  fetcher.submit({}, {
+   *    action: "query",
+   *    method: "POST",
+   *  });
+   *};
+   *
+   *return(
+   *  <Button onClick={handleQueryClick}>
+   *    Query
+   *  </Button>
+   *);
+   * ```
+   * ```ts
+   * // /app/shopify.server.ts
+   * import { LATEST_API_VERSION, shopifyApp } from "@shopify/shopify-app-remix/server";
+   *
+   * const shopify = shopifyApp({
+   *   future: {
+   *     unstable_optionalScopesApi: true,
+   *   },
+   *   // ...etc
+   * });
+   * export default shopify;
+   * export const authenticate = shopify.authenticate;
+   * ```
+   */
   query: () => Promise<ScopesDetail>;
-  request: (scopes: string[]) => Promise<void>;
-  revoke: (scopes: string[]) => Promise<RevokeResponse>;
+
+  /**
+   * Requests the merchant to grant the provided scopes for this app on this shop
+   *
+   * // TODO: I think we can omit the other error cases as they are pre-reqs (i.e. scopes deployed to Shopify, malformed access scope,.. )
+   *    We will add discoverable references to managing your scopes section to ensure developers understand what this feature and the pre-reqs are in case they discover it first via this page 
+   *    
+   * **Warning** This method performs a server-side redirect.
+   * // TODO: should we link to managing your scopes section to ensure they deployed?
+   * We recommend you do your own validation to ensure that the optional scopes being requested have not been granted.
+   *
+   * @example
+   * <caption>Request for optional scopes.</caption>
+   * <description>Call `scopes.request` to get request optional scopes.</description>
+   * ```ts
+   * // /app/routes/app.request.tsx
+   *import type { ActionFunctionArgs } from "@remix-run/node";
+   *import { json } from "@remix-run/node";
+   *
+   *import { authenticate } from "../shopify.server";
+   *import { AuthScopes } from "@shopify/shopify-api";
+   *export async function action({
+   *  request,
+   *}: ActionFunctionArgs) {
+   *  const { scopes } = await authenticate.admin(request);
+   *
+   *  const body = await request.formData();
+   *  const optionalScopesData = body.getAll("scopes") as string[];
+   *  const optionalScopes = new AuthScopes(optionalScopesData);
+   *
+   *  await scopes.request(optionalScopes.toArray());
+   *  // nothing to grant
+   *  return json({});
+   *}
+   * ```
+   * ```ts
+   * // /app/routes/**\/*.tsx
+   *const handleRequestClick = () => {
+   *  fetcher.submit({scopes: ["write_products"]}, {
+   *    action: "request",
+   *    method: "POST",
+   *  });
+   *};
+   *
+   *return(
+   *  <Button onClick={handleRequestClick}>
+   *    Request
+   *  </Button>
+   *);
+   * ```
+   * ```ts
+   * // /app/shopify.server.ts
+   * import { LATEST_API_VERSION, shopifyApp } from "@shopify/shopify-app-remix/server";
+   *
+   * const shopify = shopifyApp({
+   *   future: {
+   *     unstable_optionalScopesApi: true,
+   *   },
+   *   // ...etc
+   * });
+   * export default shopify;
+   * export const authenticate = shopify.authenticate;
+   * ```
+   */
+  request: (scopes: Scope[]) => Promise<void>;
+
+  /**
+   * Revokes the provided scopes from this app on this shop
+   *
+   * **Warning** We recommend you do your own validation to ensure that the optional scopes being requested are valid.
+   * This method throws an error if the provided optional scopes contains a required scope.
+   *
+   * @example
+   * <caption>Revoke optional scopes.</caption>
+   * <description>Call `scopes.revoke` to revoke optional scopes.</description>
+   * ```ts
+   * // /app/routes/app.revoke.tsx
+   *import type { ActionFunctionArgs } from "@remix-run/node";
+   *import { json } from "@remix-run/node";
+   *import { authenticate } from "../shopify.server";
+   *import { AuthScopes } from "@shopify/shopify-api";
+   *
+   *export async function action({
+   *  request,
+   *}: ActionFunctionArgs) {
+   *  const { scopes } = await authenticate.admin(request);
+   *
+   *  const body = await request.formData();
+   *  const scopesToRevokeData = body.getAll("scopes") as string[];
+   *  const scopesToRevoke = new AuthScopes(scopesToRevokeData);
+   *
+   *  try {
+   *    const revokedResponse = await scopes.revoke(scopesToRevoke.toArray());
+   *    console.log("Revoked response:", revokedResponse);
+   *
+   *    const queryResponse = await scopes.query();
+   *    return json({scopesDetail : queryResponse});
+   *  } catch (e) {
+   *    console.log(JSON.stringify(e));
+   *    return json({});
+   *  }
+   *}
+   * ```
+   * ```ts
+   * // /app/routes/**\/*.tsx
+   *const handleRevokeClick = () => {
+   *  fetcher.submit({scopes: ["write_products"]}, {
+   *    action: "revoke",
+   *    method: "POST",
+   *  });
+   *};
+   *
+   *return(
+   *  <Button onClick={handleRevokeClick}>
+   *    Revoke
+   *  </Button>
+   *);
+   * ```
+   * ```ts
+   * // /app/shopify.server.ts
+   * import { LATEST_API_VERSION, shopifyApp } from "@shopify/shopify-app-remix/server";
+   *
+   * const shopify = shopifyApp({
+   *   future: {
+   *     unstable_optionalScopesApi: true,
+   *   },
+   *   // ...etc
+   * });
+   * export default shopify;
+   * export const authenticate = shopify.authenticate;
+   * ```
+   */
+  revoke: (scopes: Scope[]) => Promise<ScopesRevokeResponse>;
 }
 
-export interface RevokeResponse {
-  revoked: string[];
-}
+/**
+ * `Scope` represents an [access scope](https://shopify.dev/docs/api/usage/access-scopes) handle, like "`write_products`" or "`read_orders`"
+ */
+export type Scope = string;
+
+/**
+ * `UserResult` represents the results of a user responding to a scopes request, i.e. a merchant user’s action taken when presented with a grant modal.
+ */
+export type UserResult = 'granted-all' | 'declined-all';
+
 export interface ScopesDetail {
-  granted: string[];
-  required: string[];
-  optional: string[];
+  /**
+   * The scopes that have been granted on the shop for this app
+   */
+  granted: Scope[];
+  /**
+   * The required scopes that the app has declared in its configuration
+   */
+  required: Scope[];
+  /**
+   * The optional scopes that the app has declared in its configuration
+   */
+  optional: Scope[];
+}
+
+export interface ScopesRequestResponse {
+  result: UserResult;
+  detail: ScopesDetail;
+}
+
+export interface ScopesRevokeResponse {
+  revoked: Scope[];
 }
